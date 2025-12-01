f1f0b3a2cb2a3d68d4fe2b1fc83626504534ca9c
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-01 19:51:46 +0100

    pt2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day01/src/main.rs
+++ Day01/src/main.rs
@@ -1,31 +1,50 @@
 use std::fs;
 
+fn get_num(input: &str) -> i32
+{
+    let number: i32;
+    if input.as_bytes()[0] == b'L' {
+        let n = input.get(1..input.len()).expect("Input parsing error");
+        number = n.parse::<i32>().expect("Cannot convert to number") * -1;
+    }
+    else {
+        let n = input.get(1..input.len()).expect("Input parsing error");
+        number = n.parse::<i32>().expect("Cannot convert to number");
+    }
+    return number;
+}
+
 fn main() {
     let mut contents = fs::read_to_string("./src/puzzle2.txt").expect("File does not exist");
     contents.pop();
     let split = contents.split("\n");
-    // println!("{:#?}", split);
 
     let mut count = 0;
-
     let mut tick: i32 = 50;
+    let mut last = 50;
 
-    let mut number: i32;
     for input in split {
-        if input.as_bytes()[0] == b'L' {
-            let n = input.get(1..input.len()).expect("Input parsing error");
-            number = n.parse::<i32>().expect("Cannot convert to number") * -1;
-        }
-        else {
-            let n = input.get(1..input.len()).expect("Input parsing error");
-            number = n.parse::<i32>().expect("Cannot convert to number");
-        }
+        let number: i32 = get_num(input);
+
+        let add = (number / 100).abs();
+        count += add;
+        let number = number % 100;
 
         tick += number;
+        let add = (tick / 101).abs();
+        if add != 0 {
+            count += add;
+        }
+
         tick %= 100;
         if tick == 0 {
             count += 1;
         }
+        if last * tick < 0 {
+            count += 1;
+        }
+        if tick < 0 { tick += 100 }
+        last = tick;
     }
 
     println!("Generated 0'es: {:?}", count)
