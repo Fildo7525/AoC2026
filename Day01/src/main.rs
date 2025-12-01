efa5e5a9f0f8159912f7e50711ea7c6bf109a930
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-01 18:54:28 +0100

    pt1
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day01/src/main.rs
+++ Day01/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut contents = fs::read_to_string("./src/puzzle2.txt").expect("File does not exist");
+    contents.pop();
+    let split = contents.split("\n");
+    // println!("{:#?}", split);
+
+    let mut count = 0;
+
+    let mut tick: i32 = 50;
+
+    let mut number: i32;
+    for input in split {
+        if input.as_bytes()[0] == b'L' {
+            let n = input.get(1..input.len()).expect("Input parsing error");
+            number = n.parse::<i32>().expect("Cannot convert to number") * -1;
+        }
+        else {
+            let n = input.get(1..input.len()).expect("Input parsing error");
+            number = n.parse::<i32>().expect("Cannot convert to number");
+        }
+
+        tick += number;
+        tick %= 100;
+        if tick == 0 {
+            count += 1;
+        }
+    }
+
+    println!("Generated 0'es: {:?}", count)
+
 }
