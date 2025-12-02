e57b959ccc472de2532d70c4aa9e9f186d21e6e8
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-02 16:01:55 +0100

    pt2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day02/src/main.rs
+++ Day02/src/main.rs
@@ -1,3 +1,4 @@
+use core::str;
 use std::fs;
 
 #[derive(Debug)]
@@ -22,16 +23,52 @@ fn get_ranges(input: &str) -> Vec<Range>
     return out;
 }
 
+fn check_sequence(sequence: &[u8], s: &Vec<u8>) -> bool
+{
+    let mut idx: usize = 0;
+
+    let mut pts = Vec::new();
+    while idx + sequence.len() <= s.len().try_into().unwrap() {
+        let ss = &s[idx..idx+sequence.len()];
+        pts.push(ss);
+        idx += sequence.len();
+    }
+
+    return pts.iter().all(|&pt| return pt == sequence);
+}
+
+fn separate_and_check(num: i128, snum: &Vec<u8>) -> i128
+{
+
+    for i in 1..snum.len()/2+1 {
+        if snum.len() % i != 0 {
+            continue;
+        }
+
+        let seq = &snum[0..i];
+        if check_sequence(seq, snum) {
+            return num;
+        }
+    }
+    return 0;
+}
+
 
 fn check_number(num: i128) -> i128
 {
     let snum: Vec<u8> = num.to_string().bytes().collect();
-    if snum.len() % 2 != 0 { return 0; }
+    if snum.len() % 2 != 0 {
+        return separate_and_check(num, &snum);
+    }
     let f = &snum[0..snum.len()/2];
     let l = &snum[snum.len()/2..snum.len()];
 
-    if f == l { return num; }
-    return 0;
+    if f == l {
+        return num;
+    }
+
+    return separate_and_check(num, &snum);
+    // return 0;
 }
 
 fn main() {
@@ -43,7 +80,8 @@ fn main() {
 
     for range in ranges {
         for n in range.start..range.end+1 {
-            palindromes += check_number(n);
+            let invalid = check_number(n);
+            palindromes += invalid;
         }
     }
 
