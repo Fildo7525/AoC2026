53155b7e07cb1f671160f7cade51f3e0c90a2831
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-03 18:14:42 +0100

    pt2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day03/src/main.rs
+++ Day03/src/main.rs
@@ -6,20 +6,38 @@ fn main() {
     let mut sum: i128 = 0;
 
     for line in contents.split("\n") {
-
         let b: Vec<char> = line.chars().collect();
-        let max = b[..b.len()-1].iter().max_by_key(|&k| k).expect("No max found");
-        let max_pos = b.iter().position(|k| k == max).expect("Max out of range");
 
-        let sub: Vec<&char> = b[max_pos+1..b.len()].iter().collect();
-        println!("sub: {:?}", sub);
-        let second_max = b[max_pos+1..b.len()].iter().max_by_key(|&k| k).expect("No other numbers after max");
+        let mut sorted: Vec<char> = b[0..b.len()-12].to_vec();
+        sorted.sort_by(|a, b| b.cmp(a));
 
-        let mut num: i32 = (*max).to_digit(10).expect("No max num") as i32;
-        num *= 10;
-        num = num + (*second_max).to_digit(10).expect("No sencond max") as i32;
+        // println!("Origin: {:?}", b);
+        // println!("Sorted: {:?}", sorted);
 
+        let max = &sorted[0];
+        let mut last_pos = b.iter().position(|k| *k == *max).expect("Max out of range") + 1;
+        let mut num: i128 = (*max).to_digit(10).expect("No max num") as i128;
+
+        for i in (0..11).rev() {
+            let sub: Vec<&char> = b[last_pos..b.len()-i].iter().collect();
+            // println!("Sub({:?},{:?}): {:?}", last_pos, b.len()-i, sub);
+            let second_max = *sub.iter().max_by_key(|k| ***k).expect("No other numbers after max");
+            // println!("Second max: {:}", second_max);
+            last_pos += sub.iter().position(|k| **k == *second_max).expect("Max out of range") + 1;
+            // println!("Last pos: {:}", last_pos);
+
+            num *= 10;
+            num = num + (*second_max).to_digit(10).expect("No sencond max") as i128;
+
+        }
+
+        // println!("Num: {:}", num);
         sum += num as i128;
+
+
+        // let sub: Vec<&char> = b[max_pos+1..b.len()].iter().collect();
+        // let second_max = b[max_pos+1..b.len()].iter().max_by_key(|&k| k).expect("No other numbers after max");
+
     }
 
     println!("Joltage: {:}", sum);
