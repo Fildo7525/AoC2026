5739c0913e98ecab88c68382178e35135f5547f1
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-06 00:32:54 +0100

    pt2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day05/src/main.rs
+++ Day05/src/main.rs
@@ -1,3 +1,4 @@
+use std::time::SystemTime;
 use std::fs;
 use std::ops::{Deref, DerefMut};
 
@@ -14,6 +15,14 @@ impl Range {
         return self.start <= value && value <= self.end;
     }
 
+    fn get_size(&self) -> u128 {
+        return self.end - self.start + 1u128;
+    }
+
+    fn clone(&self) -> Range
+    {
+        return Range{ start: self.start.clone(), end: self.end.clone() };
+    }
 }
 
 struct Ranges(Vec<Range>);
@@ -25,6 +34,37 @@ impl Ranges {
             // println!("Checking {} in {},{}", value, r.start, r.end);
             r.contains(value)
             });
+    }
+
+    fn combine(&mut self) -> Ranges
+    {
+        self.sort_by(|a, b| a.start.cmp(&b.start) );
+
+        let mut new_ranges = Ranges(Vec::new());
+        new_ranges.push(self.0[0].clone());
+
+        for r in &self.0 {
+            if r.end <= new_ranges.last().unwrap().end { continue; }
+
+            if r.start-1 <= new_ranges.last().unwrap().end {
+                new_ranges.last_mut().unwrap().end = r.end;
+            }
+            else {
+                new_ranges.push(r.clone());
+            }
+        }
+
+        return new_ranges;
+    }
+
+    fn size(&self) -> u128
+    {
+        let mut total: u128 = 0;
+        for r in &self.0 {
+            total += r.get_size();
+        }
+
+        return total;
     }
 }
 
@@ -41,28 +81,52 @@ impl DerefMut for Ranges {
     }
 }
 
-fn main() {
+fn main()
+{
     let mut contents = fs::read_to_string("./src/puzzle2.txt").expect("Cannot read the file");
     contents.pop();
 
     let lines: Vec<&str> = contents.split('\n').collect();
-    let mut ranges: Ranges = Ranges(Vec::new());
-    let mut finished = false;
-    let mut fresh_count = 0;
+    let pt = 2;
 
-    for line in lines {
-        if line.is_empty() { finished = true; continue; }
+    if pt == 1 {
+        let mut ranges: Ranges = Ranges(Vec::new());
+        let mut fresh_count = 0;
+        let mut finished = false;
 
-        if !finished {
-            let split: Vec<u128> = line.split("-").map(|sn| sn.parse().unwrap()).collect();
-            // println!("Adding range {};{}", split[0], split[1]);
-            ranges.push(Range{start: split[0], end: split[1]});
+        for line in lines {
+            if line.is_empty() { finished = true; continue; }
+
+            if !finished {
+                let split: Vec<u128> = line.split("-").map(|sn| sn.parse().unwrap()).collect();
+                // println!("Adding range {};{}", split[0], split[1]);
+                ranges.push(Range{start: split[0], end: split[1]});
+            }
+
+            else if ranges.contains(line.parse::<u128>().expect("Cannot convert to u128")) {
+                fresh_count += 1;
+            }
         }
-
-        else if ranges.contains(line.parse::<u128>().expect("Cannot convert to u128")) {
-            fresh_count += 1;
-        }
+        println!("Fresh count: {:?}", fresh_count);
     }
 
-    println!("Fresh count: {:?}", fresh_count);
+    else {
+        let start = SystemTime::now();
+        let mut ranges: Ranges = Ranges(Vec::new());
+
+        for line in lines {
+            if line.is_empty() { break; }
+
+            let split: Vec<u128> = line.split("-").map(|sn| sn.parse().unwrap()).collect();
+            let range = Range{start: split[0], end: split[1]};
+            ranges.push(range);
+        }
+
+        let new_ranges = ranges.combine();
+
+        let total: u128 = new_ranges.size();
+        let duration = SystemTime::now().duration_since(start).unwrap();
+
+        println!("Total: {} in {:?}", total, duration);
+    }
 }
