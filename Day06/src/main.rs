337157a9099444506d109ca7a54bfb1e496b6e2b
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-06 18:34:29 +0100

    pt2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day06/src/main.rs
+++ Day06/src/main.rs
@@ -1,4 +1,5 @@
-use std::{char::from_digit, fs};
+use std::fs;
+
 
 struct Column {
     numbers: Vec<i128>,
@@ -26,23 +27,43 @@ impl Column
 
         return 0;
     }
+}
 
-    fn execute_pt2(&self)
+#[derive(Debug)]
+struct SColumn {
+    numbers: Vec<i128>,
+    operation: String,
+    block_size: usize,
+    start_idx: usize,
+}
+
+impl SColumn
+{
+    fn execute(&self) -> i128
     {
-        let mut snums: Vec<String> = self.numbers.iter().map(|n| n.to_string()).collect();
-        let longest = snums.iter().map(|s| s.len()).max().unwrap_or(0);
-
-        for sn in &mut snums {
-            if sn.len() == longest { continue; }
-
-            let spacing = " ".repeat(longest - sn.len());
-            let padded = format!("{}{}", spacing, sn);
-            *sn = padded;
+        if self.operation == "*" {
+            let mut out = 1;
+            for n in &self.numbers {
+                out *= n;
+            }
+            println!("Sub result for *: {}", out);
+            return out;
         }
+        else if self.operation == "+" {
+            let mut out = 0;
+            for n in &self.numbers {
+                out += n;
+            }
+            println!("Sub result for +: {}", out);
+            return out;
+        }
+
+        return 0;
     }
 }
 
-fn main() {
+fn pt1()
+{
     let mut contents = fs::read_to_string("./src/puzzle2.txt").expect("Cannot read the file");
     contents.pop();
 
@@ -90,4 +111,63 @@ fn main() {
     }
 
     println!("Result: {}", result);
+}
+
+fn pt2()
+{
+    let mut contents = fs::read_to_string("./src/puzzle2.txt").expect("Cannot read the file");
+    contents.pop();
+
+    let lines: Vec<&str> = contents.split('\n').collect();
+    let mut columns: Vec<SColumn> = Vec::new();
+
+    let operations = lines.last().unwrap().chars().collect::<Vec<char>>();
+    let mut last_op_idx = 0;
+    loop {
+        let op = operations[last_op_idx..].iter().find(|c| c != &&' ');
+        match op {
+            Some(c) => {
+                let next_op_idx = operations[last_op_idx+1..].iter().position(|x| x != &' ').unwrap_or(operations.len() - last_op_idx);
+                println!("Found op {} at idx {}, next_op_idx: {}", c, last_op_idx,  next_op_idx);
+                let nums: Vec<i128> = vec![0; next_op_idx];
+                println!("Vecor: {:?}", nums);
+
+                columns.push(SColumn{ numbers: nums, operation: c.to_string(), block_size: next_op_idx, start_idx: last_op_idx});
+                last_op_idx += next_op_idx + 1;
+                if last_op_idx >= operations.len() {
+                    break;
+                }
+            },
+            None => break,
+        }
+    }
+
+    for line in lines.iter().take(lines.len()-1)
+    {
+        let l = line.chars().collect::<Vec<char>>();
+        for op in &mut columns {
+            let nums: Vec<u8> = l[op.start_idx..op.start_idx+op.block_size].iter().map(|c| c.to_digit(10).unwrap_or(255) as u8 ).collect();
+            // println!("Nums for col {}: {:?}", op.operation, nums);
+
+            for (idx, n) in nums.iter().enumerate() {
+                if *n == 255 { continue; }
+                op.numbers[idx] = op.numbers[idx] * 10 + (*n as i128);
+            }
+        }
+    }
+
+    let mut result: i128 = 0;
+    for op in &columns {
+        let r = op.execute();
+        result += r;
+    }
+
+    println!("Result: {}", result);
+}
+
+
+fn main()
+{
+    // pt1();
+    pt2();
 }
