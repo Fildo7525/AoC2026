7fec8b456261baf48a3caaf742a642142fedc1a6
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-07 20:51:09 +0100

    pt1
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day07/src/main.rs
+++ Day07/src/main.rs
@@ -1,9 +1,117 @@
 use std::fs;
+use std::collections::VecDeque;
+
+#[derive(Debug, Clone, Copy)]
+struct Position {
+    row: i32,
+    col: i32,
+}
+
+impl Position {
+    // fn add(&self, other: &Position) -> Position {
+    //     Position {
+    //         row: self.row + other.row,
+    //         col: self.col + other.col,
+    //     }
+    // }
+
+    fn char_at(&self, field: &Vec<&str>) -> char {
+        // field[self.row as usize].chars().nth(self.col as usize).unwrap_or('\0')
+        let line = field.get(self.row as usize).unwrap_or(&"\0");
+        line.chars().nth(self.col as usize).unwrap_or('\0')
+    }
+
+    fn is_in(&self, dequeue: &VecDeque<Position>) -> bool {
+        for pos in dequeue {
+            if pos.row == self.row && pos.col == self.col {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn down(&self) -> Position
+    {
+        Position {
+            row: self.row + 1,
+            col: self.col,
+        }
+    }
+
+    fn left(&self) -> Position
+    {
+        Position {
+            row: self.row,
+            col: self.col - 1
+        }
+    }
+
+    fn right(&self) -> Position
+    {
+        Position {
+            row: self.row,
+            col: self.col + 1,
+        }
+    }
+}
+
+fn locate_start(lines: &Vec<&str>) -> Option<(usize, usize)> {
+    for (i, line) in lines.iter().enumerate() {
+        if let Some(j) = line.find('S') {
+            return Some((i, j));
+        }
+    }
+    None
+}
+
+fn simulate_beams(field: &Vec<&str>, start: Position) {
+    let mut positions: VecDeque<Position> = VecDeque::new();
+    positions.push_back(start);
+    let mut splits: i128 = 0;
+
+    while !positions.is_empty() {
+        let pos = positions.pop_front().unwrap();
+        // println!("At position: ({}, {}) in field {}", pos.row, pos.col, pos.char_at(field));
+
+
+        if pos.down().char_at(field) == '^' {
+            let left = pos.down().left();
+            let right = pos.down().right();
+            let down = pos.down();
+            println!("Splitting beam ({},{}) to ({},{}), ({},{})", down.row, down.col, left.row, left.col, right.row, right.col);
+
+            if !left.is_in(&positions) && !pos.left().is_in(&positions) {
+                positions.push_back(left);
+            }
+            if !right.is_in(&positions) && !pos.right().is_in(&positions) {
+                positions.push_back(right);
+            }
+
+            splits += 1;
+        }
+        else {
+            let d = pos.down();
+            if d.char_at(field) == '\0' {
+                // println!("Beam exited the field at ({}, {})", d.row, d.col);
+                continue;
+            }
+            positions.push_back(pos.down());
+        }
+        // println!("Beams remaining: {:?}", positions/* .len() */);
+    }
+
+    println!("Total splits: {}", splits);
+}
 
 fn main() {
     let mut contents = fs::read_to_string("./src/puzzle2.txt").expect("Cannot read the file");
     contents.pop();
 
     let lines: Vec<&str> = contents.split('\n').collect();
-    println!("Number of lines: {}", lines.len());
+    if let Some((row, col)) = locate_start(&lines) {
+        let start = Position { row: row as i32, col: col as i32 };
+        simulate_beams(&lines, start);
+    }
+
+
 }
